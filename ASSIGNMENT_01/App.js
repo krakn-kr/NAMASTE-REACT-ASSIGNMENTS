f35da662337d2009858157cde467ddeacf54ab67
krashokkumarnaidu@gmail.com <krashokkumarnaidu@gmail.com>
2023-10-14 11:40:43 +0530

    1_Intoduction

---
+++ ASSIGNMENT_01/App.js
@@ -0,0 +1,31 @@
+const heading = React.createElement(
+  "h1",
+  {
+    id: "title",
+  },
+  "Namasthe Everyone"
+);
+const heading1 = React.createElement(
+  "h1",
+  {
+    id: "title",
+  },
+  "heading 1"
+);
+const headin2 = React.createElement(
+  "h2",
+  {
+    id: "title",
+  },
+  "heading 2"
+);
+const container = React.createElement(
+  "div",
+  {
+    id: "container",
+  },
+  [heading, heading1, headin2]
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
+console.log(heading);
